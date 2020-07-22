d029da2843d49db33662c22758ccd4b878d5ca56
Ranoiaetep <55516229+Ranoiaetep@users.noreply.github.com>
2020-07-22 11:56:47 -0700

    Update Problem_94.cpp

--- Problem_94.cpp
+++ Problem_94.cpp
@@ -1,14 +1,10 @@
-#include <iostream>
-#include <cstdlib>
-#include <cmath>
-
 bool isIntTri(long long base, long long side)
 {
     std::lldiv_t baseQ = std::lldiv(base, 2);
-    if(baseQ.rem)
-    {
-        return false;
-    }
+//    if(baseQ.rem)
+//    {
+//        return false;
+//    }
     long long heightSq = side * side - baseQ.quot * baseQ.quot;
     long long height = std::round(std::pow(heightSq, 0.5));
     return height * height == heightSq;
@@ -18,7 +14,7 @@ bool isIntTri(long long base, long long side)
 int main()
 {
     long long sum = 0;
-    for(long long i = 3; i < 333333334; i++)
+    for(long long i = 4; i < 333333334; i+=2)
     {
         if(isIntTri(i, i+1))
         {
