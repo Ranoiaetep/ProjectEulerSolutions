2a93789ff496580ef349b773c590ff737a95a118
Ranoiaetep <55516229+Ranoiaetep@users.noreply.github.com>
2020-08-01 15:39:13 -0700

    Update Problem_026.cpp

--- Problem_026/Problem_026.cpp
+++ Problem_026/Problem_026.cpp
@@ -5,31 +5,32 @@
 
 int main()
 {
-    std::vector<size_t> length;
-    for(int i = 1; i < 10; i++)
+    std::vector<std::pair<size_t, int>> length;
+    for(int i = 1; i < 5000; i++)
     {
         std::vector<std::pair<int, int>> pairs;
-        std::cout << "\n" << i << ": ";
-        int remainder = 0;
+        //std::cout << "\n" << i << ": ";
+        std::div_t qr = {0, 0};
         int temp = 10;
         do
         {
-            int quotient = temp / i;
-            remainder = temp % i;
-            std::pair<int, int> pair = std::make_pair(quotient, remainder);
+            qr = std::div(temp, i);
+            std::pair<int, int> pair = std::make_pair(qr.quot, qr.rem);
             if(std::find(pairs.begin(), pairs.end(), pair) == pairs.end())
             {
-                std::cout << quotient << ", ";
+                //std::cout << quotient << ", ";
                 pairs.push_back(pair);
             }
             else
             {
                 break;
             }
-            temp = remainder * 10;
+            temp = qr.rem * 10;
         }
-        while(remainder != 0);
-        length.push_back(pairs.size());
+        while(qr.rem != 0);
+        length.emplace_back(std::pair<size_t, int>(pairs.size(),i));
+        if(i % 1000 == 0) std::cout << i << "\n";
     }
-    std::cout << "\n\n" << std::max_element(length.begin(), length.end()) - length.begin() + 1 << "\n";
+    auto it = std::max_element(length.begin(), length.end());
+    std::cout << "\n\n" << it-> first + 1 << " : " << it -> second - 1 << "\n";
 }
