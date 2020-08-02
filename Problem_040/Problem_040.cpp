9c8bcbdcc616509f574f228b5163c0f1f837122e
Ranoiaetep <55516229+Ranoiaetep@users.noreply.github.com>
2020-08-01 21:29:41 -0700

    Update Problem_040.cpp

--- Problem_040/Problem_040.cpp
+++ Problem_040/Problem_040.cpp
@@ -4,14 +4,14 @@
 int main()
 {
     std::string result = "";
-    for(int i = 0; i < 1000001; i++)
+    for(int i = 0; i <= 1000000; i++)
     {
         result += std::to_string(i);
     }
     int product = 1;
-    for(int i = 1; i < 1000001; i *= 10)
+    for(int i = 1; i <= 1000000; i *= 10)
     {
-        product *= result[i] - 48;
+        product *= result[i] - '0';
     }
     std::cout << product << "\n";
 }
