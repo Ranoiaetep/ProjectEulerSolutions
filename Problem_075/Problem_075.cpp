0df7aaf598046d7d47cb6d8e1d54f8875af46a4a
Ranoiaetep <55516229+Ranoiaetep@users.noreply.github.com>
2020-11-06 22:45:04 -0800

    Create Problem_075.cpp

---
+++ Problem_075/Problem_075.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+std::map<int, int> map;
+const int MAX = 15000000;
+struct triangle
+{
+	int A;
+	int B;
+	int C;
+};
+
+void Transform(triangle tri)
+{
+	if (tri.A + tri.B + tri.C > MAX) return;
+	for(int i = tri.A + tri.B + tri.C; i <= MAX; i += tri.A + tri.B + tri.C)
+	{
+		++map[i];
+	}
+	Transform(triangle{
+		+ 1 * tri.A - 2 * tri.B + 2 * tri.C,
+		+ 2 * tri.A - 1 * tri.B + 2 * tri.C,
+		+ 2 * tri.A - 2 * tri.B + 3 * tri.C});
+	Transform(triangle{
+		+ 1 * tri.A + 2 * tri.B + 2 * tri.C,
+		+ 2 * tri.A + 1 * tri.B + 2 * tri.C,
+		+ 2 * tri.A + 2 * tri.B + 3 * tri.C});
+	Transform(triangle{
+		- 1 * tri.A + 2 * tri.B + 2 * tri.C,
+		- 2 * tri.A + 1 * tri.B + 2 * tri.C,
+		- 2 * tri.A + 2 * tri.B + 3 * tri.C});
+}
+
+int main(int argc, const char * argv[])
+{
+	triangle Origin{3,4,5};
+	Transform(Origin);
+	std::cout << std::count_if(map.begin(), map.end(), [](auto a){
+		return a.second == 1;
+	}) << "\n\n";
+}
